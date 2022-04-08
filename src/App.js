eebbd73e9f1fe123e56b7cf9d1dbe9e9cc8f9e45
kitch-finn <fin@kakao.com>
2022-04-08 16:44:22 +0900

    add useInput /w validate

--- src/App.js
+++ src/App.js
@@ -3,20 +3,43 @@ import "./styles.css";
 import { useState } from "react";
 import ReactDOM from "react-dom";
 
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    const {
+      target: { value }
+    } = event;
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
+  };
+  return { value, onChange };
+};
+
 export default function App() {
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
+
   const [item, setItem] = useState(1);
   const incrementItem = () => setItem(item + 1);
   const decrementItem = () => setItem(item - 1);
+
   return (
     <div className="App">
       <h1>Hello {item}</h1>
       <h2>Start editing to see some magic happen!</h2>
       <button onClick={incrementItem}> increment </button>
       <button onClick={decrementItem}> decrement </button>
+      <br />
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
-// class hook start here
+// class this.state start here
 // class AppUgly extends React.Component{
 //   state = {
 //     item: 1
